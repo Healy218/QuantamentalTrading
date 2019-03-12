733e94236b024f4ed0da11dbfb9c11e6961ecb56
Matt Healy <42188555+Healy218@users.noreply.github.com>
2019-03-11 21:51:25 -0500

    Added basic json search function

--- QuantamentalTools/TweetsFileMaker.py
+++ QuantamentalTools/TweetsFileMaker.py
@@ -1,8 +1,17 @@
 import re
 import pandas as pd
 import numpy as np
-import Twitter
+import twitter
 
-def init()[
+#Fill in your twitter developer information to run the api
+api = twitter.Api(consumer_key=[consumer key],
+                  consumer_secret=[consumer secret],
+                  access_token_key=[access token],
+                  access_token_secret=[access token secret])
+#search for given twitter searches, returns a json list for tweet RNN
+GetSearch(term=none, return_json=True)
 
-]
+#opens a stream for implementation after tweet RNN has been trained
+GetStreamFilter('import twitter lists to here')
+
+
