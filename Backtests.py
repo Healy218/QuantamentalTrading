20f51d7e7bc658fb13c7f59d282751187f415c3d
Matt Healy <42188555+Healy218@users.noreply.github.com>
2019-03-26 15:25:22 -0500

    Added imports and comments

--- Backtests.py
+++ Backtests.py
@@ -1,6 +1,17 @@
 import pandas as pd
 import numpy as np
+import scipy
+import patsy
+import pickle
+import scipy.sparse
+import matplotlib.pyplot as plt
 
-init[
+from statistics import median
+from scipy.stats import gaussian_kde
+from statsmodels.formula.api import ols
+from tqdm import tqdm
 
-]
+#using barra data directories, build a backtest
+barra_dir = '../../path/to/file'
+
+data = {}
